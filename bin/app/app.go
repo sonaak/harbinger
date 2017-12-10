ae52a941757356916dcded3f3ce297af66e8e614
Knight Fu <knight.w.fu@gmail.com>
2017-12-10 00:17:24 -0500

    adds web server

--- bin/app/app.go
+++ bin/app/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"flag"
 	"time"
+	"github.com/gin-gonic/gin"
 )
 
 func Setup() (*App, error) {
@@ -17,6 +18,15 @@ type App struct {}
 
 
 func (app *App) Run() error {
+
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	go r.Run()
 
 	for range time.Tick(1 * time.Second) {
 		glog.Info("Printing hello world")
