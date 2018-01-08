2f911d9f4023f73793136235d87d9b7acfe87ce2
Knight Fu <knight.w.fu@gmail.com>
2018-01-07 22:28:36 -0500

    adds test for sub rng do

--- subscr_ring_test.go
+++ subscr_ring_test.go
@@ -1,6 +1,9 @@
 package harbinger
 
-import "testing"
+import (
+	"testing"
+	"sort"
+)
 
 
 
@@ -70,5 +73,137 @@ func TestSubscriptionRing_AddNext(t *testing.T) {
 				expectId, actualId)
 		}
 		rng.Next()
+	}
+}
+
+
+func TestSubscriptionRing_Remove(t *testing.T) {
+	subscriptions := []*subscription {
+		&subscription{
+			id: 1,
+		},
+		&subscription{
+			id: 5,
+		},
+		&subscription{
+			id: 8,
+		},
+		&subscription{
+			id: 15,
+		},
+		&subscription{
+			id: 0,
+		},
+	}
+
+	rng := NewSubscriptionRing(subscriptions[0])
+	for _, subs := range subscriptions[1:] {
+		rng.Add(subs)
+	}
+	if !rng.Remove(subscriptions[2]) {
+		t.Error("expect removal of an added node to result in removal")
+	}
+
+	if rng.Len() != 4 {
+		t.Errorf("expect length after removal to be 4")
+	}
+
+	for i := 0; i < len(subscriptions); i++ {
+		if i != 2 {
+			expectId := subscriptions[i].id
+			actualId := rng.current.Value.(*subscription).id
+			if expectId != actualId {
+				t.Errorf("expect and actual subscription ids don't match: (expect: %d, actual: %d)",
+					expectId, actualId)
+			}
+			rng.Next()
+		}
+	}
+
+	if rng.Remove(subscriptions[2]) {
+		t.Error("expect removal of a removed/non-existent node to result in no-op")
+	}
+
+	if rng.Len() != 4 {
+		t.Errorf("expect a no-op removal to result in unchanged length")
+	}
+}
+
+
+func TestSubscriptionRing_RemoveHead(t *testing.T) {
+	subscriptions := []*subscription {
+		&subscription{
+			id: 1,
+		},
+		&subscription{
+			id: 5,
+		},
+		&subscription{
+			id: 8,
+		},
+		&subscription{
+			id: 15,
+		},
+		&subscription{
+			id: 0,
+		},
+	}
+
+	rng := NewSubscriptionRing(subscriptions[0])
+	for _, subs := range subscriptions[1:] {
+		rng.Add(subs)
+	}
+
+	if !rng.Remove(subscriptions[0]) {
+		t.Error("expect removal of head to succeed")
+	}
+
+	if rng.Len() != 4 {
+		t.Errorf("expect removal of head to change number of elts in ring (expected: 4, actual: %d)", rng.Len())
+	}
+
+	headId := rng.head.Value.(*subscription).id
+	if headId != 5 {
+		t.Errorf("expect removal of head/current to change to next (expected: 5, actual: %d)", headId)
+	}
+}
+
+
+func TestSubscriptionRing_Do(t *testing.T) {
+	subscriptions := []*subscription {
+		&subscription{
+			id: 1,
+		},
+		&subscription{
+			id: 5,
+		},
+		&subscription{
+			id: 8,
+		},
+		&subscription{
+			id: 15,
+		},
+		&subscription{
+			id: 0,
+		},
+	}
+
+	rng := NewSubscriptionRing(subscriptions[0])
+	for _, subs := range subscriptions[1:] {
+		rng.Add(subs)
+	}
+
+	processedIds := []int{}
+	rng.Do(func(s *subscription){
+		processedIds = append(processedIds, int(s.id))
+	})
+
+	sort.Ints(processedIds)
+	expectedList := []int{0, 1, 5, 8, 15,}
+	for i, e := range expectedList {
+		if processedIds[i] != e {
+			t.Errorf("expect subscription %d to be processed", e)
+			return
+		}
 	}
 }
