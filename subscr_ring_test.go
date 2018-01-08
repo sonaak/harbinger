5e5ecade13dcad3cbd4075110729ba6ceea7110e
Knight Fu <knight.w.fu@gmail.com>
2018-01-07 22:07:53 -0500

    adds test for add and next

--- subscr_ring_test.go
+++ subscr_ring_test.go
@@ -24,6 +24,51 @@ func TestNewSubscriptionRing(t *testing.T) {
 	}
 
 	if rng.Len() != 1 {
-		t.Error("expect length to be 1 (actual: %d)", rng.Len())
+		t.Errorf("expect length to be 1 (actual: %d)", rng.Len())
+	}
+}
+
+
+func TestSubscriptionRing_AddNext(t *testing.T) {
+	subscriptions := []*subscription {
+		&subscription{
+			id: 1,
+		},
+		&subscription{
+			id: 5,
+		},
+		&subscription{
+			id: 8,
+		},
+		&subscription{
+			id: 15,
+		},
+		&subscription{
+			id: 0,
+		},
+	}
+
+	rng := NewSubscriptionRing(subscriptions[0])
+	for _, subs := range subscriptions[1:] {
+		rng.Add(subs)
+	}
+
+	if rng.Len() != 5 {
+		t.Errorf("expect there to be 5 elements on ring (actual: %d)", rng.Len())
+	}
+
+	for i := 0; i < len(subscriptions); i++ {
+		headId := rng.head.Value.(*subscription).id
+		if headId != 1 {
+			t.Errorf("expect the head to be unchanged (actual id: %d)", headId)
+		}
+
+		expectId := subscriptions[i].id
+		actualId := rng.current.Value.(*subscription).id
+		if expectId != actualId {
+			t.Errorf("expect and actual subscription ids don't match: (expect: %d, actual: %d)",
+				expectId, actualId)
+		}
+		rng.Next()
 	}
 }
